9fda6d22aaf7939106654e6dbc37648c352acaff
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-05-15 08:19:45 -0500

    update include path to match pizza test

--- code/tests/cases/test_copy.c
+++ code/tests/cases/test_copy.c
@@ -11,7 +11,7 @@
  * Copyright (C) 2024 Fossil Logic. All rights reserved.
  * -----------------------------------------------------------------------------
  */
-#include <fossil/test/framework.h>
+#include <fossil/pizza/framework.h>
 
 #include "fossil/code/app.h"
 
