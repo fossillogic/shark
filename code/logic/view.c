8e9b5ea77e193657c099e5b91532161140162eaf
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-11-17 23:26:00 -0600

    this time

--- code/logic/view.c
+++ code/logic/view.c
@@ -778,6 +778,7 @@ static void format_meson_line(ccstring line) {
 
     // Count leading spaces/tabs for indent
     size_t indent_len = 0;
+    size_t indent = 0;
     while (i < len && (line[i] == ' ' || line[i] == '\t')) {
         indent_len++;
         if (line[i] == '\t')
