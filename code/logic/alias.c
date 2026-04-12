81dab9eea8294e98bf2bffaa3ccac37a3ab17f2e
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2026-04-12 14:04:36 -0500

    Update alias.c

--- code/logic/alias.c
+++ code/logic/alias.c
@@ -60,7 +60,7 @@ static int fossil_spino_alias_set(const char* name, const char* cmd, bool global
         return 0;
     }
     if (_fossil_spino_alias_count >= FOSSIL_SPINO_ALIAS_MAX) {
-        fossil_io_fprintf(FOSSIL_ERROR, "Alias table full\n");
+        fossil_io_fprintf(FOSSIL_STDERR, "Alias table full\n");
         return -1;
     }
     fossil_spino_alias_t* alias = &_fossil_spino_aliases[_fossil_spino_alias_count++];
