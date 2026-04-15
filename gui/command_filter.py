7c065baeb26a913464e33da90e5a26ca79f4a813
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2026-04-14 19:35:28 -0500

    Update command_filter.py

--- gui/command_filter.py
+++ gui/command_filter.py
@@ -2,7 +2,7 @@ ALLOWED_COMMANDS = [
     "show", "copy", "move", "delete", "rename", "create",
     "search", "archive", "compare", "sync", "watch",
     "rewrite", "introspect", "grammar", "cryptic", "split",
-    "perm", "pipe", "alias", "undo", "link", "dedupe",
+    "perm", "pipe", "undo", "link", "dedupe",
     "help", "version"
 ]
 
